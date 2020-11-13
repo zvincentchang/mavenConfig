efc775176fa2eeb946d50b874b6df0ecb55b4fb2
zvincentchang <zvincentchang@gmail.com>
2020-11-13 10:02:35 +0800

    Revised HelloSevlet Mapping to BeanDemo

--- mavenspringConfig/src/main/java/HelloServlet.java
+++ mavenspringConfig/src/main/java/HelloServlet.java
@@ -13,7 +13,7 @@ import demo.example.*;
 /**
  * Servlet implementation class HelloServlet
  */
-@WebServlet("/HelloServlet")
+@WebServlet("/beandemo")
 public class HelloServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
--- mavenspringConfig/src/main/java/config/WebAppInitializer.java
+++ mavenspringConfig/src/main/java/config/WebAppInitializer.java
@@ -12,13 +12,15 @@ public class WebAppInitializer implements WebApplicationInitializer {
 
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
-        AnnotationConfigWebApplicationContext appContext = new AnnotationConfigWebApplicationContext();
+        
+    	AnnotationConfigWebApplicationContext appContext = new AnnotationConfigWebApplicationContext();
         appContext.register(WebMvcConfig.class);
 
         ServletRegistration.Dynamic dispatcher = servletContext.addServlet(
                 "SpringDispatcher", new DispatcherServlet(appContext));
         dispatcher.setLoadOnStartup(1);
         dispatcher.addMapping("/");
+        
     }
 
 }
